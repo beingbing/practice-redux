cd6a68c0b47a36580e35eaa511754ed8fa353cfe
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-03-22 06:42:46 +0530

    handling multiple state properties in redux store

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,6 +6,7 @@ const Counter = () => {
   const dispatch = useDispatch();
   // subscribing store for specified data
   const counter = useSelector(state => state.counter);
+  const showCounter = useSelector(state => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: 'increment' });
@@ -19,12 +20,14 @@ const Counter = () => {
     dispatch({ type: 'decrement' });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={() => increaseHandler(5)}>Increment by 5</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
   console.log('state: ',state, ' action: ', action);
   if (action.type === 'increment') {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter
     };
   }
 
@@ -12,13 +13,22 @@ const counterReducer = (state = { counter: 0 }, action) => {
     console.log('inside increase reducer');
     return {
       counter: state.counter + action.value,
+      showCounter: state.showCounter
     }
   }
 
   if (action.type === 'decrement') {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter
     };
+  }
+
+  if (action.type === 'toggle') {
+    return {
+      counter: state.counter,
+      showCounter : !state.showCounter
+    }
   }
 
   return state;
